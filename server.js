47f01d3fd629e5323528f0074d3b5ecb51c3a295
wisnuaga <wisnu.aga@gmail.com>
2020-03-29 00:09:42 +0700

    percobaan ke 4

--- server.js
+++ server.js
@@ -25,9 +25,6 @@ app.get('/', (req, res) => {
 });
 
 app.post('/webhook', (req, res) => {
-  const message = "halo aga!, ini webhook post";
-  console.log(message);
-
   let body = req.body;
 
   // Checks this is an event from a page subscription
@@ -40,6 +37,18 @@ app.post('/webhook', (req, res) => {
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        console.log(webhook_event.message)
+      } else if (webhook_event.postback) {
+        console.log(webhook_event.postback)
+      }
     });
 
     // Returns a '200 OK' response to all requests
@@ -53,9 +62,6 @@ app.post('/webhook', (req, res) => {
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
-  const message = "halo aga!, ini webhook get";
-  console.log(message);
-
   // Your verify token. Should be a random string.
   let VERIFY_TOKEN = "coba1234";
 
