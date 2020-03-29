69a4f90dce4827e17849ca9a8970f7c2471129d4
wisnuaga <wisnu.aga@gmail.com>
2020-03-29 22:42:55 +0700

    percobaan 4 3/29/2020

--- index.js
+++ index.js
@@ -33,7 +33,9 @@ app.post('/webhook', (req, res) => {
                 // Gets the message. entry.messaging is an array, but
                 // will only ever contain one message, so we get index 0
                 let webhook_event = entry.messaging[0];
+                console.log("===atas===")
                 console.log(webhook_event);
+                console.log("===bawah===")
             });
 
             // Returns a '200 OK' response to all requests
